605764beb4e2f8dfdb5f4499e7f9ee822374b0bc
Wiktor-Jordeczka <wiktor.jordeczka.01@gmail.com>
2026-09-09 10:34:31 +0200

    fix: adjust import scripts to account for ht profile, but no ht data type

--- versions/v2/import-onecx.sh
+++ versions/v2/import-onecx.sh
@@ -33,7 +33,7 @@ export OLE_EDITION OLE_LINE_PREFIX OLE_HEADER_CT_JSON
 ## Check and set import type
 IMPORT_TYPE="base"
 
-if [[ -n "${4:-}" && "${4:-}" =~ ^(all|base|ai|bookmark|assignment|parameter|permission|mfe|ms|product|slot|tenant|theme|welcome|workspace|menu|ht)$ ]]; then
+if [[ -n "${4:-}" && "${4:-}" =~ ^(all|base|ai|bookmark|assignment|parameter|permission|mfe|ms|product|slot|tenant|theme|welcome|workspace|menu)$ ]]; then
   IMPORT_TYPE="${4}"
 fi
 
@@ -174,72 +174,72 @@ fi
 ## IMPORT OneCX data
 cd "$import_start_dir/onecx-data" || exit 1
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|slot|product|ms|mfe|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|slot|product|ms|mfe)$ ]]; then
   pushd product-store > /dev/null
-  if [[ "$IMPORT_TYPE" =~ ^(all|base|product|ht)$ ]]; then
+  if [[ "$IMPORT_TYPE" =~ ^(all|base|product)$ ]]; then
     bash ./import-products.sh "${1:-}" "${2:-}"
   fi
-  if [[ "$IMPORT_TYPE" =~ ^(all|base|mfe|ht)$ ]]; then
+  if [[ "$IMPORT_TYPE" =~ ^(all|base|mfe)$ ]]; then
     bash ./import-microfrontends.sh "${1:-}" "${2:-}"
   fi
-  if [[ "$IMPORT_TYPE" =~ ^(all|base|ms|ht)$ ]]; then
+  if [[ "$IMPORT_TYPE" =~ ^(all|base|ms)$ ]]; then
     bash ./import-microservices.sh "${1:-}" "${2:-}"
   fi
-  if [[ "$IMPORT_TYPE" =~ ^(all|base|slot|ht)$ ]]; then
+  if [[ "$IMPORT_TYPE" =~ ^(all|base|slot)$ ]]; then
     bash ./import-slots.sh "${1:-}" "${2:-}"
   fi
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|parameter|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|parameter)$ ]]; then
   pushd parameter > /dev/null
   bash ./import-parameters.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|permission|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|permission)$ ]]; then
   pushd permission > /dev/null
   bash ./import-permissions.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|assignment|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|assignment)$ ]]; then
   pushd permission-assignment > /dev/null
   bash ./import-assignments.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|tenant|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|tenant)$ ]]; then
   pushd tenant > /dev/null
   bash ./import-tenants.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|theme|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|theme)$ ]]; then
   pushd theme > /dev/null
   bash ./import-themes.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|workspace|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|workspace)$ ]]; then
   pushd workspace > /dev/null
   bash ./import-workspaces.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|base|menu|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|base|menu)$ ]]; then
   pushd workspace > /dev/null
   bash ./import-menu.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|welcome|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|welcome)$ ]]; then
   pushd welcome > /dev/null
   bash ./import-welcome-images.sh "${1:-}" "${2:-}"
   popd > /dev/null
 fi
 
-if [[ "$IMPORT_TYPE" =~ ^(all|bookmark|ht)$ ]]; then
+if [[ "$IMPORT_TYPE" =~ ^(all|bookmark)$ ]]; then
   pushd bookmark > /dev/null
   bash ./import-bookmarks.sh "${1:-}" "${2:-}"
   popd > /dev/null
